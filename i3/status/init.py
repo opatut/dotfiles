16485e6e8058e55db26ca5e925a3c8679fd7ed4e
Paul Bienkowski <pb@opatut.de>
2016-04-19 17:31:56 +0200

    various stuff

--- i3/status/init.py
+++ i3/status/init.py
@@ -6,35 +6,35 @@ from i3pystatus import Status
 
 status = Status(standalone=True)
 
-status.register("clock", format="🕗 %X")
-status.register("clock", format="📅 %a %-d %b")
+#status.register("clock", format="%X ")
+status.register("clock", format="%a %-d %b %X ")
 
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp", format="{temp:.0f}°C")
+status.register("temp", format="{temp:02.0f}°C ")
 
 # The battery monitor has many formatting options, see README for details
 
 # This would also display a desktop notification (via dbus) if the percentage
 # goes below 5 percent while discharging. The block will also color RED.
-status.register("battery", format="🔋 {status} {percentage:.2f}% {remaining:%E%hh:%Mm}", alert=True, alert_percentage=5, status=dict(DIS="(-)", CHR="(+)", FULL="(=)"))
+status.register("battery", format="{percentage:05.2f}% {status}", alert=True, alert_percentage=5, status=dict(DIS="", CHR="", FULL=""))
 
-status.register("network", interface="eth0", format_up="eth {v4}", format_down="")
+status.register("network", interface="eth0", format_up="eth {v4} ", format_down="no eth ")
 
 # Has all the options of the normal network and adds some wireless specific things
 # like quality and network names.
-status.register("network", interface="wlan0", format_down="no wifi", format_up="🌍 {essid} {v4}",)
+status.register("network", interface="wlan0", format_down="no wifi ", format_up="{essid} {v4} ",)
 
-status.register("cpu_usage", format="💻 {usage}%")
-status.register("mem", format="💾 {percent_used_mem:.0f}%", warn_percentage=70, alert_percentage=85)
+status.register("cpu_usage", format="{usage:02.0f}% ")
+status.register("mem", format="{percent_used_mem:02.0f}% ", warn_percentage=70, alert_percentage=85)
 
 # Shows pulseaudio default sink volume
-status.register("pulseaudio", format="🔊 {volume}",)
+status.register("pulseaudio", format="{volume:02.f}% ",)
 
 #status.register("dota2wins", steam_api_key="53AACE69A9E67ADE6560BCBCEB035C42", interval=30*60, steamid="opatut", matches=20, screenname='', format='🎮 {win_percent:.0f}%', good_threshold=55, bad_threshold=45)
 
 # Shows mpd status
 # Format:
 # Cloud connected▶Reroute to Remain
-#status.register("mpd", format="🎶 {title} - {album} \[{status}\]", status=dict(pause="Paused", play="Playing", stop="Stopped"))
+status.register("mpd", format="({status}) {artist} - {title} ({album}) ", status=dict(pause="", play="", stop=""))
 
 status.run()
--- zsh/zshrc.d/00-aliases.sh
+++ zsh/zshrc.d/00-aliases.sh
@@ -28,6 +28,14 @@ alias less='less -S'
 unset GREP_OPTIONS
 alias grep='grep --color=auto'
 alias csv='tabview -w max'
+alias nis='npm install --save'
+alias mysql='mysql --auto-rehash'
+alias e='gvim --servername vim --remote-silent'
+
+alias date1='date +"%Y%m%d%H%M%S"'
+alias date2='date +"%Y-%m-%d-%H-%M-%S"'
+alias date3='date +"%Y%m%d-%H%M%S"'
+alias date4='date +"%Y-%m-%d %H:%M:%S"'
 
 export PATH="/usr/lib/colorgcc/bin:$HOME/.android/sdk/platform-tools:$HOME/.android/sdk/tools:$HOME/.gem/ruby/2.1.0/bin:$HOME/.bin/:$(ruby -e 'puts Gem.user_dir')/bin:.:$HOME/.npm-global/bin/:$PATH"
 export EDITOR=vim
@@ -35,3 +43,5 @@ export WINE_DEBUG=-all
 export BROWSER=chromium
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk
 export NODE_REPL_HISTORY_FILE="$HOME/.nodehistory"
+export NODE_PATH="$HOME/.npm-global/lib/node_modules"
+export ANDROID_HOME=/opt/android-sdk

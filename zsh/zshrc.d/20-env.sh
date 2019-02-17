3c9413b954c2d75a9de30a61884c958a86783a80
Paul Bienkowski <pb@opatut.de>
2019-02-17 15:40:55 +0100

    zsh: remove legacy stuff

--- zsh/zshrc.d/20-env.sh
+++ zsh/zshrc.d/20-env.sh
@@ -1,6 +1,6 @@
 unset GREP_OPTIONS
 
-
+export QT_QPA_PLATFORMTHEME="qt5ct"
 export TERM=xterm-256color
 export GOPATH=$HOME/src/go
 export EDITOR=vim
@@ -11,7 +11,5 @@ export NODE_REPL_HISTORY_FILE="$HOME/.nodehistory"
 export NODE_PATH="$HOME/.npm-global/lib/node_modules"
 export ANDROID_HOME=/opt/android-sdk
 export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
-export DEV_TOOL=eval
 export HISTSIZE=100000000
 export SAVEHIST=$HISTSIZE
-export KOPS_STATE_STORE=s3://kops.mesaic.io
